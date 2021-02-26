a8cef7a811d6b539a6526593bcdb21f384904415
paulo sandim <paulo@cacdigital.com.br>
2021-02-26 17:54:09 -0300

    validando o resultado esperado com Rspec

--- spec/vingadores_spec.rb
+++ spec/vingadores_spec.rb
@@ -15,9 +15,47 @@ end
 # A Classe só foi criada por conta do teste abaixo
 describe Vingadores do
     it 'deve adicionar um vingador' do
-        vi = Vingadores.new
+        vingadores = Vingadores.new
 
-        vi.put('Homem Aranha')
-        expect(vi.list).to eql ['Homem Aranha']
+        vingadores.put('Homem-aranha')
+        expect(vingadores.list).to include 'Homem-aranha'
+    end
+
+    it 'deve adicionar uma lista de vingadores' do
+       vingadores = Vingadores.new
+       vingadores.put('Thor')
+       vingadores.put('Hulk')
+       vingadores.put('Homem-aranha')
+       
+       resultado = vingadores.list.size > 0
+       expect(resultado).to be true
+    end
+
+    it 'Thor deve ser o primeiro da lista' do
+        vingadores = Vingadores.new
+        
+        vingadores.put('Thor')
+        vingadores.put('Hulk')
+        vingadores.put('Homem-aranha')
+
+        expect(vingadores.list).to start_with('Thor')
+    end
+
+    it 'Homem de Ferro deve ser o último da lista' do
+        vingadores = Vingadores.new
+        
+        vingadores.put('Thor')
+        vingadores.put('Hulk')
+        vingadores.put('Homem-aranha')
+        vingadores.put('Homem de Ferro')
+
+        expect(vingadores.list).to end_with('Homem de Ferro')
+    end
+
+    it 'deve conter o sobrenome' do
+        vingador = 'Peter Parker'
+
+        expect(vingador).to match(/Peter Parker/)
+        expect(vingador).not_to match(/Paulo/)
     end
 end
